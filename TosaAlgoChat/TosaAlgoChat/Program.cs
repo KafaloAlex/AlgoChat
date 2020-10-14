402ade70a75c88921a6d23157feeb8941c0900b7
Alex <61283644+KafaloAlex@users.noreply.github.com>
2020-10-14 13:06:13 +0000

    Création du projet

---
+++ TosaAlgoChat/TosaAlgoChat/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TosaAlgoChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ma Team
+            int sizeTeam = Convert.ToInt32(Console.ReadLine());
+            string typeTeam = Console.ReadLine();
+
+            //Adversaire Var
+            int sizeAdv = Convert.ToInt32(Console.ReadLine());
+            string typeAdv = Console.ReadLine();
+        }
+    }
+}
